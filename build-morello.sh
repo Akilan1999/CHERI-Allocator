4d96bdf354c916719bba32fdd09cc7e7528aead0
Akilan <me@akilan.io>
2024-03-07 10:51:00 +0000

    fixed build script

--- build-morello.sh
+++ build-morello.sh
@@ -1,6 +1,6 @@
 # Building shared object files for morello
 
-cc -shared -march=purecap-benchmark -fPIC alloc.c -o alloc.so
-cc -shared -march=purecap-benchmark -fPIC alloc.c -o regularalloc.so
+cc -g -Wall -shared -mabi=purecap-benchmark -fPIC alloc.c -o alloc.so
+cc -g -Wall -shared -mabi=purecap-benchmark -fPIC alloc.c -o regularalloc.so
 
 
