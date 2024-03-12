c76799291eb175665966cf804d7e61e52c98bd8d
Akilan <me@akilan.io>
2024-03-12 12:01:10 +0000

    enabled program to run back in sequence

--- Programs/stddefines.h
+++ Programs/stddefines.h
@@ -162,7 +162,7 @@ static inline void *MALLOCCONTIGOUS(size_t size)
     }
 
     void *ptr = mmap(NULL, size,
-                    PROT_READ | PROT_WRITE, MAP_SHARED,
+                    PROT_READ | PROT_WRITE, MAP_PRIVATE,
                     fd, offset);
     if(ptr == MAP_FAILED)
     {
