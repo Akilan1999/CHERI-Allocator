b51f401f9cc105c9883febba8cd139ba2c51c4ce
Akilan <me@akilan.io>
2024-04-07 13:55:19 +0100

    debug push

--- dpdk/lib/eal/freebsd/eal_hugepage_info.c
+++ dpdk/lib/eal/freebsd/eal_hugepage_info.c
@@ -158,7 +158,7 @@ eal_hugepage_info_read(void)
 		return -1;
 	}
 
-		printf(internal_conf->hugepage_dir);
+	    printf("%c", internal_conf->hugepage_dir);
 
 	memcpy(hpi, tmp_hpi, sizeof(internal_conf->hugepage_info));
 
