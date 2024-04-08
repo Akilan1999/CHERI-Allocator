a8d79ac01881365dc03f73f2179fcd3e6365cc84
Akilan <me@akilan.io>
2024-04-08 15:01:54 +0100

    added fix to run hello world

--- dpdk/run-contigko.sh
+++ dpdk/run-contigko.sh
@@ -19,6 +19,9 @@ sudo cp contigmem.ko /boot/modules/
 # Run the new contig kernel module
 sudo kldload /boot/modules/contigmem.ko
 
+# Get back to the DPDK directory
+cd /home/akilan/CHERI-Allocator/dpdk
+
 # Run HelloWorld
 sudo ./build-hybrid/examples/dpdk-helloworld
 
