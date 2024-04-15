9d0c2ee4067dfc60cd398007a1dd35aff6a0800e
Akilan <me@akilan.io>
2024-04-15 10:17:10 +0100

    debug push

--- dpdk/kernel/freebsd/Malloc/Malloc.c
+++ dpdk/kernel/freebsd/Malloc/Malloc.c
@@ -85,7 +85,10 @@ Alloc(struct thread *td, struct syscall_alloc *uap)
 
 	// td->td_retval[0] = uap;
 
-	*uap->size = 10;
+
+    printf("Changing size \n"); 
+
+	// *uap->size = 10;
 
 	// *arg = uap;
 
