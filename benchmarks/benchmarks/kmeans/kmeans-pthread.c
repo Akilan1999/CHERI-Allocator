0acee403821a4a2e9377a1af86bc41b02a749543
Akilan <me@akilan.io>
2024-04-24 17:41:29 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/kmeans-pthread.c
+++ benchmarks/benchmarks/kmeans/kmeans-pthread.c
@@ -39,12 +39,12 @@
 
 #include "coz.h"
 
-// #define malloc      MALLOCCHERI
-// #define free        FREECHERI
+#define malloc      MALLOCCHERI
+#define free        FREECHERI
 
 #define DEF_NUM_POINTS 100000
 #define DEF_NUM_MEANS 100
-#define DEF_DIM 7
+#define DEF_DIM 20
 #define DEF_GRID_SIZE 1000
 
 // #define DEF_NUM_POINTS 1000
@@ -262,8 +262,8 @@ int main(int argc, char **argv)
    
    // Extra code snippet added 
    // printf("Initial alloc called\n");
-   //INITAlloc();
-   //INITREGULARALLOC();
+   INITAlloc();
+   INITREGULARALLOC();
    
    int num_procs, curr_point;
    int i;
