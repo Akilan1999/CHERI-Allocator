741538ca38c9caf31fab4d439d28f8745990acb7
Akilan <me@akilan.io>
2024-04-29 20:28:27 +0100

    test whole program

--- pyplot/TLB-DTLB-walk.py
+++ pyplot/TLB-DTLB-walk.py
@@ -28,23 +28,24 @@ ypoints = np.array([3625,
 
 xpoints = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
 
-ypoints1 = np.array([2001205195,
-    2037350408,
-    2077998800,
-    2064361816,
-    2370004326,
-    2366505116,
-    2433485997,
-    2388982491,
-    2622710065,
-    2231849577,
-    2213896383,
-    1971144730,
-    2279336623,
-    2384236727,
-    2159066740,
-    1872922315,
-    2046840068])
+ypoints1 = np.array([6798,
+         2144,
+         2327,
+         2234,
+         2779,
+         3046,
+         2824,
+         2762,
+         2857,
+         2404,
+         3236,
+         2473,
+         2517,
+         2558,
+         3065,
+         3222,
+         3235,
+         4939])
 
 xpoints1 = np.array([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17])
 
@@ -58,7 +59,7 @@ access to at least the Level 1 data or unified TLB.
 Each access to a TLB entry is counted including multiple accesses caused by single instructions
 such as LDM or STM.
 '''
-plt.title("Level 1 data TLB access, read \n ARM Performance counter: L1D_TLB_RD \n This counter counts each access counted by \n L1D_TLB that is a Memory-read operation. \n Kmeans C program with Cluster size 3")
+plt.title("DTLB walk access, read \n ARM Performance counter: L1D_TLB_RD \n This counter counts each access counted by \n L1D_TLB that is a Memory-read operation. \n Kmeans C program with Cluster size 3")
 
 plt.xlabel("time in seconds")
 plt.ylabel("L1 DTLB reads")
