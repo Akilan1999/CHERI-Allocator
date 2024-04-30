42757b3f8be0264a7e008ffdc17a50700df70970
Akilan <me@akilan.io>
2024-04-30 01:54:35 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/build.sh
+++ benchmarks/benchmarks/kmeans/build.sh
@@ -1,3 +1,3 @@
 git pull origin main
 cc -g -Wall -o kmeans-pthread.out -mabi=purecap-benchmark -lpthread kmeans-pthread.c
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./kmeans-pthread.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./kmeans-pthread.out
