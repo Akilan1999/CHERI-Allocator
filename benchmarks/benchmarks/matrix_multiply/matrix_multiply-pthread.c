73754eb8ad4fa9b8a96f07a5e1fd548e46e0e572
Akilan <me@akilan.io>
2024-05-05 18:21:20 +0100

    test whole program

--- benchmarks/benchmarks/matrix_multiply/matrix_multiply-pthread.c
+++ benchmarks/benchmarks/matrix_multiply/matrix_multiply-pthread.c
@@ -117,8 +117,8 @@ void matrixmult_splitter(void *data_in)
     for (i = 0; i < num_procs; i++)
     {
         dprintf("Inner loop \n");
-        int ret_val;
-        CHECK_ERROR(pthread_join(tid[i], (void **)(void*)&ret_val) != 0);
+        // int ret_val;
+        // CHECK_ERROR(pthread_join(tid[i], (void **)(void*)&ret_val) != 0);
 	//   CHECK_ERROR(ret_val != 0);
     }
     dprintf("Free step \n");
