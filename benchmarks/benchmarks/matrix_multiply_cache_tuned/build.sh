89afdf01ec495449614ccdbfd0b69faac85e1996
Akilan <me@akilan.io>
2024-05-05 17:40:55 +0100

    test whole program

--- benchmarks/benchmarks/matrix_multiply_cache_tuned/build.sh
+++ benchmarks/benchmarks/matrix_multiply_cache_tuned/build.sh
@@ -1,3 +1,3 @@
 git pull origin main
-cc -g -Wall -o matrix_multiply.out -mabi=purecap-benchmark -lpthread matrix_multiply.out.c
+cc -g -Wall -o matrix_multiply.out -mabi=purecap-benchmark -lpthread matrix_multiply.c
 sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply.out
