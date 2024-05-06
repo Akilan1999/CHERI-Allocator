5746b33d04634ab4b037cbf28a5f479b80d577cb
Akilan <me@akilan.io>
2024-05-06 22:08:01 +0100

    test whole program

--- benchmarks/benchmarks/histogram/build.sh
+++ benchmarks/benchmarks/histogram/build.sh
@@ -2,20 +2,20 @@ git pull origin main
 cc -g -Wall -o histogram-pthread.out -mabi=purecap-benchmark -lpthread histogram-pthread.c
 
 # Run multiple PMCStat paramters
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/small.bmp -o ll_cache_miss_rd_histogram_small.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/small.bmp -o L2D_TLB_histogram_small.txt
-sudo pmcstat -d -w 1 -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/small.bmp -o DTLB_WALK_histogram_small.txt
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/small.bmp -o L1D_TLB_RD_histogram_small.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/small.bmp -o L2D_TLB_histogram_small.txt
+sudo pmcstat -d -w 1 -O ll_cache_miss_rd_histogram_small.txt -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/small.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_small.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/small.bmp
+sudo pmcstat -d -w 1 -O DTLB_WALK_histogram_small.txt -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/small.bmp
+sudo pmcstat -d -w 1 -O L1D_TLB_RD_histogram_small.txt -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/small.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_small.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/small.bmp
 
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/med.bmp -o ll_cache_miss_rd_histogram_med.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/med.bmp -o L2D_TLB_histogram_med.txt
-sudo pmcstat -d -w 1 -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/med.bmp -o DTLB_WALK_histogram_med.txt
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/med.bmp -o L1D_TLB_RD_histogram_med.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/med.bmp -o L2D_TLB_histogram_med.txt
+sudo pmcstat -d -w 1 -O ll_cache_miss_rd_histogram_med.txt -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/med.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_med.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/med.bmp
+sudo pmcstat -d -w 1 -O DTLB_WALK_histogram_med.txt -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/med.bmp
+sudo pmcstat -d -w 1 -O L1D_TLB_RD_histogram_med.txt -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/med.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_med.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/med.bmp
 
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/large.bmp -o ll_cache_miss_rd_histogram_large.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp -o L2D_TLB_histogram_large.txt
-sudo pmcstat -d -w 1 -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/large.bmp -o DTLB_WALK_histogram_large.txt
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/large.bmp -o L1D_TLB_RD_histogram_large.txt
-sudo pmcstat -d -w 1 -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp -o L2D_TLB_histogram_large.txt
+sudo pmcstat -d -w 1 -O ll_cache_miss_rd_histogram_large.txt -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/large.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_large.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp
+sudo pmcstat -d -w 1 -O DTLB_WALK_histogram_large.txt -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/large.bmp
+sudo pmcstat -d -w 1 -O L1D_TLB_RD_histogram_large.txt -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/large.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_large.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp
