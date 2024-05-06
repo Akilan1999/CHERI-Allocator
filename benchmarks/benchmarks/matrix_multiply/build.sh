74cb8da5ea55954001a3b7fef7c48ac34f8dd057
Akilan <me@akilan.io>
2024-05-06 22:43:58 +0100

    test whole program

--- benchmarks/benchmarks/matrix_multiply/build.sh
+++ benchmarks/benchmarks/matrix_multiply/build.sh
@@ -3,17 +3,17 @@ cc -g -Wall -o matrix_multiply-pthread.out -mabi=purecap-benchmark -lpthread mat
 
 
 # Run multiple PMCStat paramters
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 200 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 200 > test.out
-sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 200 > test.out
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 200 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 200 > test.out
+sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 200 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 200 -create_files > test.out
+sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 200 -create_files > test.out
+sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 200 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 200 -create_files > test.out
 
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 1000 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 1000 > test.out
-sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 1000 > test.out
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 1000 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 1000 > test.out
+sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 1000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 1000 -create_files > test.out
+sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 1000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 1000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 1000 -create_files > test.out
 
 # sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 10000 > test.out
 # sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 10000 > test.out
