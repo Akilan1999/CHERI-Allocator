680e0822750955ab75a8f86e8341c8eefadddc4f
Akilan <me@akilan.io>
2024-05-06 22:09:38 +0100

    test whole program

--- benchmarks/benchmarks/histogram/build.sh
+++ benchmarks/benchmarks/histogram/build.sh
@@ -2,7 +2,7 @@ git pull origin main
 cc -g -Wall -o histogram-pthread.out -mabi=purecap-benchmark -lpthread histogram-pthread.c
 
 # Run multiple PMCStat paramters
-sudo pmcstat -d -w 1 -O ll_cache_miss_rd_histogram_small.txt -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/small.bmp
+sudo pmcstat -d -w 1 -p -O ll_cache_miss_rd_histogram_small.txt ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/small.bmp
 sudo pmcstat -d -w 1 -O L2D_TLB_histogram_small.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/small.bmp
 sudo pmcstat -d -w 1 -O DTLB_WALK_histogram_small.txt -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/small.bmp
 sudo pmcstat -d -w 1 -O L1D_TLB_RD_histogram_small.txt -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/small.bmp
@@ -18,4 +18,4 @@ sudo pmcstat -d -w 1 -O ll_cache_miss_rd_histogram_large.txt -p ll_cache_miss_rd
 sudo pmcstat -d -w 1 -O L2D_TLB_histogram_large.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp
 sudo pmcstat -d -w 1 -O DTLB_WALK_histogram_large.txt -p DTLB_WALK ./histogram-pthread.out histogram_datafiles/large.bmp
 sudo pmcstat -d -w 1 -O L1D_TLB_RD_histogram_large.txt -p L1D_TLB_RD ./histogram-pthread.out histogram_datafiles/large.bmp
-sudo pmcstat -d -w 1 -O L2D_TLB_histogram_large.txt -p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp
+sudo pmcstat -d -w 1 -O L2D_TLB_histogram_large.txt-p L2D_TLB ./histogram-pthread.out histogram_datafiles/large.bmp
