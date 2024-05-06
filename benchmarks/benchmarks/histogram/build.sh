acf28ef85bfcaa563cbd4fe82012c96d18b67f71
Akilan <me@akilan.io>
2024-05-06 21:49:36 +0100

    test whole program

--- benchmarks/benchmarks/histogram/build.sh
+++ benchmarks/benchmarks/histogram/build.sh
@@ -1,3 +1,3 @@
 git pull origin main
 cc -g -Wall -o histogram-pthread.out -mabi=purecap-benchmark -lpthread histogram-pthread.c
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./kmeans-pthread.out
+sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./histogram-pthread.out histogram_datafiles/small.bmp
