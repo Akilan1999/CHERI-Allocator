db68fccd71c5d047755a5dfd8b07a256ef6fad66
Akilan <me@akilan.io>
2024-05-06 22:49:34 +0100

    test whole program

--- benchmarks/benchmarks/matrix_multiply/build.sh
+++ benchmarks/benchmarks/matrix_multiply/build.sh
@@ -15,8 +15,8 @@ sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 1000 -create_fil
 sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 1000 -create_files > test.out
 sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 1000 -create_files > test.out
 
-sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 5000 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 5000 > test.out
-sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 5000 > test.out
-sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 5000 > test.out
-sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 5000 > test.out
+sudo pmcstat -d -w 1 -p ll_cache_miss_rd ./matrix_multiply-pthread.out 5000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 5000 -create_files > test.out
+sudo pmcstat -d -w 1 -p DTLB_WALK ./matrix_multiply-pthread.out 5000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L1D_TLB_RD ./matrix_multiply-pthread.out 5000 -create_files > test.out
+sudo pmcstat -d -w 1 -p L2D_TLB ./matrix_multiply-pthread.out 5000 -create_files > test.out
