60341876e459a1a3d460b5ea245536e156a28a33
Akilan <me@akilan.io>
2024-07-18 17:35:14 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/kmeans-pthread.c
+++ benchmarks/benchmarks/kmeans/kmeans-pthread.c
@@ -50,7 +50,7 @@
 #define DEF_NUM_POINTS 10000
 #define DEF_NUM_MEANS 100
 #define DEF_DIM 40
-#define DEF_GRID_SIZE 100
+#define DEF_GRID_SIZE 1000
 
 // #define DEF_NUM_POINTS 1000
 // #define DEF_NUM_MEANS 10
