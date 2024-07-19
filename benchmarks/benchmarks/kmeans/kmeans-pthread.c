0d0514f98c22ae61a39af6d7a396bde229f7d896
Akilan <me@akilan.io>
2024-07-19 09:44:30 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/kmeans-pthread.c
+++ benchmarks/benchmarks/kmeans/kmeans-pthread.c
@@ -42,7 +42,7 @@
 #define malloc      MALLOCCHERI
 #define free        FREECHERI
 
-#define DEF_NUM_POINTS 1000000
+#define DEF_NUM_POINTS 100000
 #define DEF_NUM_MEANS 100
 #define DEF_DIM 40
 #define DEF_GRID_SIZE 1000
