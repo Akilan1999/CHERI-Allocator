154dd77bc01a714a0dfae6a60608cf11615285ac
Akilan <me@akilan.io>
2024-07-19 09:49:44 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/kmeans-pthread.c
+++ benchmarks/benchmarks/kmeans/kmeans-pthread.c
@@ -42,7 +42,7 @@
 #define malloc      MALLOCCHERI
 #define free        FREECHERI
 
-#define DEF_NUM_POINTS 100000
+#define DEF_NUM_POINTS 1000000
 #define DEF_NUM_MEANS 100
 #define DEF_DIM 40
 #define DEF_GRID_SIZE 1000
@@ -269,7 +269,7 @@ int main(int argc, char **argv)
    
    // Extra code snippet added 
    // printf("Initial alloc called\n");
-   // INITAlloc();
+   INITAlloc();
    //INITREGULARALLOC();
    
    int num_procs, curr_point;
