8cd57ee18ceea45f3f6ce71cac4719ab4436a4ec
Akilan <me@akilan.io>
2024-07-26 15:16:06 +0100

    test whole program

--- benchmarks/benchmarks/kmeans/run-cluster-sizes.sh
+++ benchmarks/benchmarks/kmeans/run-cluster-sizes.sh
@@ -1,5 +1,7 @@
 sudo sh build.sh
 
-time sudo ./kmeans-pthread.out 
-
-sudo pmcstat ./kmeans-pthread.out 
+sudo time pmcstat -d -w 1 -p l1d_tlb_rd -p l2d_tlb_rd -p l1d_tlb_refill -p cpu_cycles -p dtlb_walk -p stall_backend -o kmeans-bounds-alloc-200000.txt ./kmeans-pthread.out -d 40 -c 100 -p 200000 -s 1000
+sudo time pmcstat -d -w 1 -p l1d_tlb_rd -p l2d_tlb_rd -p l1d_tlb_refill -p cpu_cycles -p dtlb_walk -p stall_backend -o kmeans-bounds-alloc-150000.txt ./kmeans-pthread.out -d 40 -c 100 -p 150000 -s 1000
+sudo time pmcstat -d -w 1 -p l1d_tlb_rd -p l2d_tlb_rd -p l1d_tlb_refill -p cpu_cycles -p dtlb_walk -p stall_backend -o kmeans-bounds-alloc-100000.txt ./kmeans-pthread.out -d 40 -c 100 -p 100000 -s 1000
+sudo time pmcstat -d -w 1 -p l1d_tlb_rd -p l2d_tlb_rd -p l1d_tlb_refill -p cpu_cycles -p dtlb_walk -p stall_backend -o kmeans-bounds-alloc-10000.txt ./kmeans-pthread.out -d 40 -c 100 -p 10000 -s 1000
+sudo time pmcstat -d -w 1 -p l1d_tlb_rd -p l2d_tlb_rd -p l1d_tlb_refill -p cpu_cycles -p dtlb_walk -p stall_backend -o kmeans-bounds-alloc-1000.txt ./kmeans-pthread.out -d 40 -c 100 -p 1000 -s 1000
